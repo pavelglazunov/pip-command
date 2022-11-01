26b154853e2fca2cc88b129bee05587121cc7ba9
pavelglazunov <p6282813@yandex.ru>
2022-11-01 13:04:55 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from io import open
+from setuptools import setup
+
+
+version = "0.0.1"
+
+setup(
+    name="pip_command",
+    version=version,
+
+    author="pavelgs",
+    author_email="p6282813@yandex.ru",
+
+    description=""
+)
